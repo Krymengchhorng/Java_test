be41b3234d930bb030cc4376cc7e3665475b0082
Krymengchhorng <mengchhorngk@gmail.com>
2024-01-09 10:23:57 +0700

    first commit

---
+++ src/information/data.java
@@ -0,0 +1,13 @@
+package information;
+
+public class data {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str1 ="idNumber :160363311",str2 ="dob :01/01/1995",str3 ="gender :M",str4 ="lastNameEn :MEN",str5 ="firstNameEn:CHENDA",str6 ="expiredDate:21/06/2024";
+		  
+		 
+		 System.out.println(str1+"\n"+str2+"\n"+str3+"\n"+str4+"\n"+str5+"\n"+str6);
+	}
+
+}
